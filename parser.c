e19f70eed257f62e3d2f8ab62b1fb3c72f419830
azullstrom <zefaniasmusic@gmail.com>
2023-03-31 17:25:50 +0200

    Part 2 begun

--- parser.c
+++ parser.c
@@ -12,7 +12,7 @@
 /**********************************************************************/
 /* Other OBJECT's METHODS (IMPORTED)                                  */
 /**********************************************************************/
-/* #include "keytoktab.h"   */       /* when the keytoktab is added   */
+#include "keytoktab.h"
 /* #include "lexer.h"       */       /* when the lexer     is added   */
 /* #include "symtab.h"      */       /* when the symtab    is added   */
 /* #include "optab.h"       */       /* when the optab     is added   */
@@ -29,21 +29,21 @@ static int  is_parse_ok=1;
 /**********************************************************************/
 /* define tokens + keywords NB: remove this when keytoktab.h is added */
 /**********************************************************************/
-enum tvalues { program = 257, id, input, output };
+// enum tvalues { program = 257, id, input, output, var, integer, begin, defined_as, number, end };
 /**********************************************************************/
 /* Simulate the token stream for a given program                      */
 /**********************************************************************/
 static int tokens[] = {program, id, '(', input, ',', output, ')', ';',
-               '$' };
+var, id, ',', id, ',', id, ':', integer, ';', begin, id, predef,
+id, '+', id, '*', number, end, '.', '$' };
 
 /**********************************************************************/
 /*  Simulate the lexer -- get the next token from the buffer          */
 /**********************************************************************/
-static int pget_token()
-{  
+static int pget_token() {  
    static int i=0;
    if (tokens[i] != '$') return tokens[i++]; else return '$';
-   }
+}
 
 /**********************************************************************/
 /*  PRIVATE METHODS for this OBJECT  (using "static" in C)            */
@@ -52,39 +52,50 @@ static int pget_token()
 /**********************************************************************/
 /* The Parser functions                                               */
 /**********************************************************************/
-static void match(int t)
-{
-   if(DEBUG) printf("\n --------In match expected: %4d, found: %4d",
-                    t, lookahead);
-   if (lookahead == t) lookahead = pget_token();
-   else {
-      is_parse_ok=0;
-      printf("\n *** Unexpected Token: expected: %4d found: %4d (in match)",
-              t, lookahead);
-      }
-   }
+static void match(int t) {
+    if(DEBUG) printf("\n --------In match expected: %4d, found: %4d", t, lookahead);
+    if (lookahead == t) lookahead = pget_token();
+    else {
+        is_parse_ok=0;
+        printf("\n *** Unexpected Token: expected: %4d found: %4d (in match)", t, lookahead);
+    }
+}
 
 /**********************************************************************/
 /* The grammar functions                                              */
 /**********************************************************************/
-static void program_header()
-{
+static void program_header() {
    if (DEBUG) printf("\n *** In  program_header");
    match(program); match(id); match('('); match(input);
    match(','); match(output); match(')'); match(';');
-   }
+}
+
+static void var_part() {
+   if (DEBUG) printf("\n *** In  var_part");
+   match(var); match(id); match(','); match(id); 
+   match(','); match(id); match(':'); match(integer); 
+   match(';');
+}
+
+static void stat_part() {
+   if (DEBUG) printf("\n *** In  stat_part");
+   match(begin); match(id); match(predef); match(id); match('+');
+   match(id); match('*'); match(number); match(end);
+   match('.');
+}
    
 /**********************************************************************/
 /*  PUBLIC METHODS for this OBJECT  (EXPORTED)                        */
 /**********************************************************************/
 
-int parser()
-{
+int parser() {
    if (DEBUG) printf("\n *** In  parser");
    lookahead = pget_token();       // get the first token
    program_header();               // call the first grammar rule
+   var_part();
+   stat_part();
    return is_parse_ok;             // status indicator
-   }
+}
 
 /**********************************************************************/
 /* End of code                                                        */
